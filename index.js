3b0a052bcc87f28b55889bc6f6194f556250b835
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-04-24 13:33:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -56,4 +56,5 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you.";
   }
   var result = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
+  cart = [];
 }
